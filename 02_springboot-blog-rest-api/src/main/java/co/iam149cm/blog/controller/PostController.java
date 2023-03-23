871bbe71f5c224c7bd9711bf27c6070ddccbe566
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-23 21:53:32 +0900

    add controller

--- 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/controller/PostController.java
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/controller/PostController.java
@@ -1,2 +1,28 @@
-package co.iam149cm.blog.controller;public class PostController {
+package co.iam149cm.blog.controller;
+
+import co.iam149cm.blog.payload.PostDto;
+import co.iam149cm.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService; // use interface to make loose coupling
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create blog post
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
 }
