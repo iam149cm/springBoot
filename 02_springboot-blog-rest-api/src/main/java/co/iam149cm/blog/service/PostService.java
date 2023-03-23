c55783806b097dec4431adac0ff80bdb585b9b01
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-23 22:23:18 +0900

    add get post list api

--- 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/service/PostService.java
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/service/PostService.java
@@ -3,6 +3,9 @@ package co.iam149cm.blog.service;
 
 import co.iam149cm.blog.payload.PostDto;
 
+import java.util.List;
+
 public interface PostService {
     PostDto createPost(PostDto postDto);
+    List<PostDto> getAllPosts();
 }
