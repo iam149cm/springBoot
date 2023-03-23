fe3b3093d89dfa4bcba82a5411ffb498a8646fdc
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-23 20:34:55 +0900

    add custom exception

--- 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/repository/PostRepository.java
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/repository/PostRepository.java
@@ -4,7 +4,7 @@ import co.iam149cm.blog.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> { // post 의 id 가 long type이기 때문
-    
+
 
 
 }
