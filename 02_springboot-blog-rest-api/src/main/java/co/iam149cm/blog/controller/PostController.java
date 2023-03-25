a980efc5edf22bf482612d9d8fa5d757c9a6ff20
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-25 21:00:40 +0900

    refactored hardcoded value for paging and sorting

--- 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/controller/PostController.java
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/controller/PostController.java
@@ -4,6 +4,7 @@ import co.iam149cm.blog.entity.Post;
 import co.iam149cm.blog.payload.PostDto;
 import co.iam149cm.blog.payload.PostResponse;
 import co.iam149cm.blog.service.PostService;
+import co.iam149cm.blog.utils.AppConstants;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -31,10 +32,10 @@ public class PostController {
     // get all posts rest api
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ) {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
