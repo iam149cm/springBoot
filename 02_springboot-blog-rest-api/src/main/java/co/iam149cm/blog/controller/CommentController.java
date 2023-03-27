072be53c9efb74fd70f5f46b7f07a72b86d9d4ac
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-27 20:03:27 +0900

    add update comment restAPI

--- 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/controller/CommentController.java
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/controller/CommentController.java
@@ -1,5 +1,6 @@
 package co.iam149cm.blog.controller;
 
+import co.iam149cm.blog.entity.Comment;
 import co.iam149cm.blog.payload.CommentDto;
 import co.iam149cm.blog.service.CommentService;
 import org.springframework.http.HttpStatus;
@@ -35,5 +36,13 @@ public class CommentController {
                                                      @PathVariable("id") long commentId) {
         CommentDto commentDto = commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDto, HttpStatus.OK);
+    }
+
+    @PutMapping("/posts/{postId}/comments/{id}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable long postId,
+                                                    @PathVariable("id") long commentId,
+                                                    @RequestBody CommentDto commentDto) {
+        CommentDto updatedComment = commentService.updateComment(postId, commentId, commentDto);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
     }
 }
--- 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/service/CommentService.java
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/service/CommentService.java
@@ -1,5 +1,6 @@
 package co.iam149cm.blog.service;
 
+import co.iam149cm.blog.entity.Comment;
 import co.iam149cm.blog.payload.CommentDto;
 
 import java.util.List;
@@ -8,4 +9,5 @@ public interface CommentService {
     CommentDto createComment(long postId, CommentDto commentDto);
     List<CommentDto> getCommentByPostId(long postId);
     CommentDto getCommentById(long postId, long commentId);
+    CommentDto updateComment(long postId, long commentId, CommentDto commentRequest);
 }
