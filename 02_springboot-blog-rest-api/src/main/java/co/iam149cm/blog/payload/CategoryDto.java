c55f3d9fe660d95b40211c9522951fa77c2a5ed1
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-03-31 11:36:38 +0900

    add categoty repository, Dto

---
+++ 02_springboot-blog-rest-api/src/main/java/co/iam149cm/blog/payload/CategoryDto.java
@@ -0,0 +1,16 @@
+package co.iam149cm.blog.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+    private long id;
+    private String description;
+
+}
