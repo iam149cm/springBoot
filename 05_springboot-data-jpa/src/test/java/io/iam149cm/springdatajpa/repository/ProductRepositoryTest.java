a027bd3825ffe4db0d6a25e20836bebfa0574bed
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-07 20:07:42 +0900

    add junit test for the JPA repository

--- 05_springboot-data-jpa/src/test/java/io/iam149cm/springdatajpa/repository/ProductRepositoryTest.java
+++ 05_springboot-data-jpa/src/test/java/io/iam149cm/springdatajpa/repository/ProductRepositoryTest.java
@@ -2,10 +2,12 @@ package io.iam149cm.springdatajpa.repository;
 
 import io.iam149cm.springdatajpa.entity.Product;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -47,4 +49,88 @@ class ProductRepositoryTest {
         // save entity to database
         Product savedProduct = repository.save(product);
     }
+
+    @Test
+    void findByIdMethod() {
+        long id = 1;
+        Product product = repository.findById(id).get();
+        System.out.println(product.toString());
+    }
+
+    @Test
+    void saveAllMethod() {
+        Product product1 = new Product();
+        product1.setName("iPhone 13");
+        product1.setDescription("Apple iPhone 13");
+        product1.setSku("IPHONE13");
+        product1.setPrice(new BigDecimal(999.99));
+        product1.setActive(true);
+        product1.setImageUrl("https://www.apple.com/iphone-13/");
+
+        Product product2 = new Product();
+        product2.setName("iPhone 13 PRO");
+        product2.setDescription("Apple iPhone 13 PRO");
+        product2.setSku("IPHONE13PRO");
+        product2.setPrice(new BigDecimal(999.99));
+        product2.setActive(true);
+        product2.setImageUrl("https://www.apple.com/iphone-13/");
+
+        Product product3 = new Product();
+        product3.setName("iPhone 13 PRO MAX");
+        product3.setDescription("Apple iPhone 13 PRO MAX");
+        product3.setSku("IPHONE13PROMAX");
+        product3.setPrice(new BigDecimal(999.99));
+        product3.setActive(true);
+        product3.setImageUrl("https://www.apple.com/iphone-13/");
+
+        repository.saveAll(List.of(product1, product2, product3));
+    }
+
+    @Test
+    void findAllMethod(){
+        List<Product> products = repository.findAll();
+        products.forEach((p) -> {
+            System.out.println("👉 "+p.getName());
+        });
+    }
+
+    @Test
+    void deleteByIdMethod(){
+        long id = 1;
+        repository.deleteById(id);
+    }
+
+    @Test
+    void deleleMethod(){
+        // find an entity by id
+        long id = 2;
+        Product product = repository.findById(id).get();
+
+        // delete entity
+        repository.delete(product);
+    }
+
+    @Test
+    void deleteAllMethod(){
+//        repository.deleteAll(); // delete all entities
+
+        Product product1 = repository.findById(6L).get();
+        Product product2 = repository.findById(7L).get();
+        repository.deleteAll(List.of(product1, product2)); // delete all entities in the list
+    }
+
+    @Test
+    void countMethod(){
+        long count = repository.count();
+        System.out.println("Total number of products: " + count);
+    }
+
+    @Test
+    void existsByIdMethod(){
+        long id = 14;
+        boolean exists = repository.existsById(id);
+        System.out.println("Product with id " + id + " 👉 exists: " + exists);
+    }
+
+
 }
