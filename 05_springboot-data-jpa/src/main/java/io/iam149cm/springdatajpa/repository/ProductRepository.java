312cae3b1042bcb24c278377914b99201b8503f3
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-07 16:13:09 +0900

    add repository

---
+++ 05_springboot-data-jpa/src/main/java/io/iam149cm/springdatajpa/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package io.iam149cm.springdatajpa.repository;
+
+import io.iam149cm.springdatajpa.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// @Repository is not necessary
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
