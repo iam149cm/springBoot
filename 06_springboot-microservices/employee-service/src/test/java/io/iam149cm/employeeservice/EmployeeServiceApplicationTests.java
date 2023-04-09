9d2065552dbb23c6e5a87c6327ee8ad90d50aa54
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-09 20:19:20 +0900

    initial commit

---
+++ 06_springboot-microservices/employee-service/src/test/java/io/iam149cm/employeeservice/EmployeeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.iam149cm.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
