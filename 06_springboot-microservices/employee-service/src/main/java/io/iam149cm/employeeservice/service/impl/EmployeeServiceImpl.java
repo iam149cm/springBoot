9cd3d8c421d2ae7e7a21c5e79971f5bec3ef2be6
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-10 16:05:53 +0900

    add microservices communication using WebClient

--- 06_springboot-microservices/employee-service/src/main/java/io/iam149cm/employeeservice/service/impl/EmployeeServiceImpl.java
+++ 06_springboot-microservices/employee-service/src/main/java/io/iam149cm/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -20,7 +21,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
     private ModelMapper mapper;
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -35,12 +37,19 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = employeeRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee", "id", id));
 
-        ResponseEntity<DepartmentDto> responseEntity =
-                restTemplate.getForEntity(
-                        "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                        DepartmentDto.class);
+//        ResponseEntity<DepartmentDto> responseEntity =
+//                restTemplate.getForEntity(
+//                        "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                        DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
+
         EmployeeDto employeeDto = mapToDto(employee);
 
         APIResponseDto apiResponseDto = new APIResponseDto();
