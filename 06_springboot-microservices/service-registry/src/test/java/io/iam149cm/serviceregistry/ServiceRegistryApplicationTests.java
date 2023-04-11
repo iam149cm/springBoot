bad58332ea52898714d71407e05c793d40eb5830
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-11 16:19:13 +0900

    initial commit

---
+++ 06_springboot-microservices/service-registry/src/test/java/io/iam149cm/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.iam149cm.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
