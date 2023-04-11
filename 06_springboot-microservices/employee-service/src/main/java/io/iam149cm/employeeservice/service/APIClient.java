67017b60eac1df55de150c78c5a573a23487d56b
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-11 21:36:27 +0900

    update to use Spring Cloud LoadBalancer

--- 06_springboot-microservices/employee-service/src/main/java/io/iam149cm/employeeservice/service/APIClient.java
+++ 06_springboot-microservices/employee-service/src/main/java/io/iam149cm/employeeservice/service/APIClient.java
@@ -7,7 +7,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "localhost:8080", value = "DEPARTMENT-SERVICE")
+// 호출 할 API 의 URL 을 명시하지 않고, application.properties 에 등록된 service id 만 전달하면
+// Feign Client 가 알아서 해당 서비스의 API 를 호출한다.
+// (Eureka Server 에 등록된 서비스만 가능하며 Eureka Client 에 내장된 로드밸런서 모듈로 로드밸런싱 한다)
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 
     // copied from DepartmentController.java
--- 06_springboot-microservices/service-registry/src/main/java/io/iam149cm/serviceregistry/ServiceRegistryApplication.java
+++ 06_springboot-microservices/service-registry/src/main/java/io/iam149cm/serviceregistry/ServiceRegistryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaServer // make this service a Eureka Server - auto configured from spring cloud 2022.0.0
+@EnableEurekaServer // make this service a Eureka Server - auto configured from spring cloud 2022.0.0
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
