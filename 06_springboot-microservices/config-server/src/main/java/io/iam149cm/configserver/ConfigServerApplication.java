a824db80214b2ddbc060040427868d2d193905bf
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-12 20:24:26 +0900

    refactored Department service to use config server

--- 06_springboot-microservices/config-server/src/main/java/io/iam149cm/configserver/ConfigServerApplication.java
+++ 06_springboot-microservices/config-server/src/main/java/io/iam149cm/configserver/ConfigServerApplication.java
@@ -2,8 +2,11 @@ package io.iam149cm.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+// this project will act as a config server and will be able to serve the configuration files from the git repository
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
