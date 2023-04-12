440eb38aafb4bf0bdd41a1dca017804a80a2033f
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-12 16:44:01 +0900

    initial commit

---
+++ 06_springboot-microservices/api-gateway/src/main/java/io/iam149cm/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.iam149cm.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
