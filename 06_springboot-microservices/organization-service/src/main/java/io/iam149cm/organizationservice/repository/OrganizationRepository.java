d4b7e2ff31a1d32d3d62a284bd527b2917d81f5d
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-14 23:41:25 +0900

    add entity, repository

---
+++ 06_springboot-microservices/organization-service/src/main/java/io/iam149cm/organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package io.iam149cm.organizationservice.repository;
+
+import io.iam149cm.organizationservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
