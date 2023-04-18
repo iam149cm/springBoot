008bec9ed0d41d6b85396c8bc3498e6036ebae7a
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-18 22:59:05 +0900

    update react front app to show data from api gateway

--- 06_springboot-microservices/react-frontend/src/component/EmployeeComponent.js
+++ 06_springboot-microservices/react-frontend/src/component/EmployeeComponent.js
@@ -18,10 +18,6 @@ class EmployeeComponent extends Component {
             this.setState({ employee : response.data.employee })
             this.setState({ department : response.data.department })
             this.setState({ organization : response.data.organization })
-
-            console.log('👉', this.state.employee)
-            console.log('👉', this.state.department)
-            console.log('👉', this.state.organization)
         })
     }
     
@@ -29,6 +25,35 @@ class EmployeeComponent extends Component {
     render() {
         return (
             <div>
+                <div className='card col-md-6 offset-md-3'>
+                    <h3 className='text-center card-header'>View Employee Details</h3>
+                    <div className='card-body'>
+                        <div className='row'>
+                            <p><strong>Employee First Name : </strong> {this.state.employee.firstName}</p>
+                            <p><strong>Employee Last Name : </strong> {this.state.employee.lastName}</p>
+                            <p><strong>Employee Email : </strong> {this.state.employee.email}</p>
+                        </div>
+                    </div>
+
+                    <h3 className='text-center card-header'>View Department Details</h3>
+                    <div className='card-body'>
+                        <div className='row'>
+                            <p><strong>Department Code : </strong> {this.state.department.departmentCode}</p>
+                            <p><strong>Department Name : </strong> {this.state.department.departmentName}</p>
+                            <p><strong>Department Description : </strong> {this.state.department.departmentDescription}</p>
+                        </div>
+                    </div>
+
+                    <h3 className='text-center card-header'>View Department Details</h3>
+                    <div className='card-body'>
+                        <div className='row'>
+                            <p><strong>Organization Code : </strong> {this.state.organization.organizationCode}</p>
+                            <p><strong>Organization Name : </strong> {this.state.organization.organizationName}</p>
+                            <p><strong>Organization Description : </strong> {this.state.organization.organizationDescription}</p>
+                        </div>
+                    </div>
+
+                </div>
                 
             </div>
         );
