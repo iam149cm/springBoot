feb6f3f0aa100fe42a8bccf83d0e11d0f7b94ec2
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-20 22:29:06 +0900

    add custom Swagger API Documentation information

--- 06_springboot-microservices/department-service/src/main/java/io/iam149cm/departmentservice/controller/DepartmentController.java
+++ 06_springboot-microservices/department-service/src/main/java/io/iam149cm/departmentservice/controller/DepartmentController.java
@@ -2,11 +2,16 @@ package io.iam149cm.departmentservice.controller;
 
 import io.iam149cm.departmentservice.dto.DepartmentDto;
 import io.iam149cm.departmentservice.service.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Department Controller",
+        description = "Department Controller Exposes REST APIs for Department Service")
 @RestController
 @RequestMapping("/api/departments")
 @AllArgsConstructor
@@ -15,6 +20,14 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     // Build save department REST API
+    @Operation(
+            summary = "Save Department",
+            description = "Save Department REST API"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Department Created Successfully"
+    )
     @PostMapping
     public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto) {
         DepartmentDto savedDepartmentDto = departmentService.saveDepartment(departmentDto);
@@ -22,6 +35,14 @@ public class DepartmentController {
     }
 
     // Build get department by code REST API
+    @Operation(
+            summary = "Get Department",
+            description = "Get Department REST API"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Department Retrieved Successfully"
+    )
     @GetMapping("/{code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("code") String departmentCode) {
         DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
--- 06_springboot-microservices/department-service/src/main/java/io/iam149cm/departmentservice/dto/DepartmentDto.java
+++ 06_springboot-microservices/department-service/src/main/java/io/iam149cm/departmentservice/dto/DepartmentDto.java
@@ -1,17 +1,33 @@
 package io.iam149cm.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+        description = "Department Dto Model"
+)
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DepartmentDto {
     private Long id;
+    @Schema(
+            description = "Department Name",
+            example = "Computer Science"
+    )
     private String departmentName;
+    @Schema(
+            description = "Department Description",
+            example = "Computer Science Department"
+    )
     private String departmentDescription;
+    @Schema(
+            description = "Department Code",
+            example = "CS"
+    )
     private String departmentCode;
 }
