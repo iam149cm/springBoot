8145dacf5a5792341020c7440298b87ea0355ec1
iam149cm <85501501+iam149cm@users.noreply.github.com>
2023-04-20 22:18:53 +0900

    add general API information in Swagger ui

--- 06_springboot-microservices/department-service/src/main/java/io/iam149cm/departmentservice/DepartmentServiceApplication.java
+++ 06_springboot-microservices/department-service/src/main/java/io/iam149cm/departmentservice/DepartmentServiceApplication.java
@@ -1,10 +1,35 @@
 package io.iam149cm.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "1.0.0",
+				contact = @Contact(
+						name = "iam149cm",
+						email = "iam149cm@email.com",
+						url = "https://github.com/iam149cm"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/iam149cm"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department Service External Documentation",
+				url = "https://github.com/iam149cm"
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
